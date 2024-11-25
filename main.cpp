c03b216770eefc8dbcc002bb9668d43058bed5a2
DotV4pe <louisbesle.sup@gmail.com>
2024-11-25 16:29:30 +0100

    1 commit

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!\n";
-  cout << "I am learning C++\n";
+  cout << "Hello World!" << endl;
+  cout << "I am learning C++" << endl;
   return 0;
 }
