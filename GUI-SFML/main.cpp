14abb1e7f9b6aa284a5d20a91f8dda6f9bfacea6
DotV4pe <louisbesle.sup@gmail.com>
2024-11-25 16:47:40 +0100

    Commit test

--- GUI-SFML/main.cpp
+++ GUI-SFML/main.cpp
@@ -4,28 +4,28 @@
 #include <cstdlib>
 
 const int cellSize = 50;
-const int gridWidth = 10;
-const int gridHeight = 10;
+const int grilleLongueur = 10;
+const int grilleHauteur = 10;
 
-std::vector<std::vector<int>> grid(gridWidth, std::vector<int>(gridHeight));
+std::vector<std::vector<int>> grille(grilleLongueur, std::vector<int>(grilleHauteur));
 
-void initializeGrid() {
+void initializegrille() {
     std::srand(std::time(0));
-    for (int x = 0; x < gridWidth; ++x) {
-        for (int y = 0; y < gridHeight; ++y) {
-            grid[x][y] = std::rand() % 2;  // Randomly initialize cells as alive or dead
+    for (int x = 0; x < grilleLongueur; ++x) {
+        for (int y = 0; y < grilleHauteur; ++y) {
+            grille[x][y] = std::rand() % 2;  // Randomly initialize cells as alive or dead
         }
     }
 }
 
-void renderGrid(sf::RenderWindow &window) {
+void rendergrille(sf::RenderWindow &window) {
     int x, y;
     
     window.clear();
     sf::RectangleShape cell(sf::Vector2f(cellSize - 1.0f, cellSize - 1.0f));
-    for (x = 0; x < gridWidth; ++x) {
-        for (y = 0; y < gridHeight; ++y) {
-            if (grid[x][y] == 1) {
+    for (x = 0; x < grilleLongueur; ++x) {
+        for (y = 0; y < grilleHauteur; ++y) {
+            if (grille[x][y] == 1) {
                 cell.setPosition(x * cellSize, y * cellSize);
                 window.draw(cell);
             }
@@ -34,23 +34,23 @@ void renderGrid(sf::RenderWindow &window) {
     window.display();
 }
 
-void calculGrid() {
+void calculgrille() {
     std::srand(std::time(0));
-    for (int x = 0; x < gridWidth; ++x) {
-        for (int y = 0; y < gridHeight; ++y) {
-            if (grid[x][y] == 0){
-                grid[x][y] = 1;
+    for (int x = 0; x < grilleLongueur; ++x) {
+        for (int y = 0; y < grilleHauteur; ++y) {
+            if (grille[x][y] == 0){
+                grille[x][y] = 1;
             } else {
-                grid[x][y] = 0;
+                grille[x][y] = 0;
             }
         }
     }
 }
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(gridWidth * cellSize, gridHeight * cellSize), "Le Jeu de la Vie");
+    sf::RenderWindow window(sf::VideoMode(grilleLongueur * cellSize, grilleHauteur * cellSize), "Le Jeu de la Vie");
     
-    initializeGrid();
+    initializegrille();
 
     while (window.isOpen()) {
         sf::Event event;
@@ -59,8 +59,8 @@ int main() {
                 window.close();
         }
         
-        renderGrid(window);
-        calculGrid();
+        rendergrille(window);
+        calculgrille();
         sf::sleep(sf::milliseconds(100));
     }
 
