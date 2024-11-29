7531c8594e8c96ac6a2b5cb50ae0c34d343359b7
DotV4pe <louisbesle.sup@gmail.com>
2024-11-29 11:44:15 +0100

    Grille v2

--- GUI-SFML/main.cpp
+++ GUI-SFML/main.cpp
@@ -1,12 +1,30 @@
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
+#include <filesystem>
 
 #include "Grille.hpp"
 
 using namespace std;
 
 int main() {
-    Grille g(10,50,50);
+    ifstream monFlux("data.txt");  //Ouverture d'un fichier en lecture
+
+    if(monFlux)
+    {
+        cout << "Tout est prêt pour la lecture." << endl;
+    }
+    else
+    {
+        cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
+    }
+
+    int hauteur,longueur, taille;
+    monFlux >> longueur;
+    monFlux >> hauteur;
+    taille = 75;
+
+    Grille g(taille,longueur,hauteur);
 
     sf::RenderWindow window(sf::VideoMode(g.getLongueur() * g.getTaille(), g.getHauteur() * g.getTaille()), "Le Jeu de la Vie");
     
