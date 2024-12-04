1364b369439db35b95faa7a41ec2ea0e0b4d5ba6
DotV4pe <louisbesle.sup@gmail.com>
2024-12-04 13:45:32 +0100

    Enlevement de la bordure des cellules

--- JeuDeLaVie.cpp
+++ JeuDeLaVie.cpp
@@ -6,7 +6,7 @@ JeuDeLaVie::~JeuDeLaVie() {}
 
 void JeuDeLaVie::affichage(Grille *g, sf::RenderWindow &window) {
     window.clear();
-    sf::RectangleShape cell(sf::Vector2f(g->getTaille() - 1.0f, g->getTaille() - 1.0f));
+    sf::RectangleShape cell(sf::Vector2f(g->getTaille(), g->getTaille()));
     for (int x = 0; x < g->get_nbColonne(); ++x) {
         for (int y = 0; y < g->get_nbLigne(); ++y) {
             cell.setPosition(x * g->getTaille(), y * g->getTaille());
@@ -14,7 +14,7 @@ void JeuDeLaVie::affichage(Grille *g, sf::RenderWindow &window) {
                 cell.setFillColor(sf::Color(255, 192, 203));
             }
             else if (g->getValeur(x,y) == 0) {
-                cell.setFillColor(sf::Color(0,0,200)); 
+                cell.setFillColor(sf::Color(0,0,0)); 
             } else if (g->getValeur(x,y) == 2) {
                 cell.setFillColor(sf::Color(255,0,0)); 
             }
