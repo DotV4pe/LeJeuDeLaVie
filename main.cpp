e23c144c5b767b92a59d4fdd96ac36530cde90d9
DotV4pe <louisbesle.sup@gmail.com>
2024-12-06 09:51:43 +0100

    main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
-#include <gtest/gtest.h>
-
 #include "./Menu/Menu.hpp"
 
 using namespace std;
 
-int main(int argc, char **argv) {
+int main() {
     Menu m;
     m.lancer();
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    return 0;
 }
