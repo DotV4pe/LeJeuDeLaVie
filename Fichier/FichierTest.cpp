19e92c8d3d6982f52da78f7a7a9b258947f331d0
DotV4pe <louisbesle.sup@gmail.com>
2024-12-06 12:37:27 +0100

    FichierTest.cpp

---
+++ Fichier/FichierTest.cpp
@@ -0,0 +1,20 @@
+#include "FichierTest.hpp"
+
+void FichierTest::getChemin(int mode) {
+    // Création d'un fichier de test temporaire
+    cheminFichier = "./Matrices/test_grid.txt";
+    std::ofstream file(cheminFichier);
+        
+    // Écriture d'une grille de test simple
+    file << "5 5\n";
+    file << "0 0 0 0 0\n";
+    file << "0 1 1 1 0\n";
+    file << "0 0 0 0 0\n";
+    file << "0 0 0 0 0\n";
+    file << "0 0 0 0 0\n";
+        
+    file.close();
+        
+    nomFichier = "test";
+    taille = 25;
+}
