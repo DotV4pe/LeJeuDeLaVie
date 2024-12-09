06b554398e5578d7fd64f69e389f6aa40dfdf29f
lilixnp <lilianpichard20@gmail.com>
2024-12-09 09:56:37 +0100

    Version Finale

--- Fichier/FichierTxt.cpp
+++ Fichier/FichierTxt.cpp
@@ -10,18 +10,20 @@ void FichierTxt::getChemin(int mode) {
     cout << "Entrez le chemin du fichier de l'état initial des cellules : ";
     cin >> nomFichier;
 
-    // Attribution d'une taille en fonction du fichier donner
-    if (nomFichier == "data") {
-        taille = 190;
-    } else if (nomFichier == "test") {
-        taille = 25;
-    } else if (nomFichier == "a") {
-        taille = 20;
-    } else if (nomFichier == "b") {
-        taille = 7;
-    } else {
-        cout << "Entrez la taille des pixels que vous souhaitez : ";
-        cin >> taille; // Si le fichier n'est pas fourni de base (data,test,a,b)
+    if (mode == 2){
+        // Attribution d'une taille en fonction du fichier donné
+        if (nomFichier == "Glider_Obstacles") {
+            taille = 190;
+        } else if (nomFichier == "test") {
+            taille = 25;
+        } else if (nomFichier == "a") {
+            taille = 20;
+        } else if (nomFichier == "b") {
+            taille = 7;
+        } else {
+            cout << "Entrez la taille des pixels que vous souhaitez : ";
+            cin >> taille; // Si le fichier n'est pas fourni de base (data,test,a,b)
+        }
     }
     cheminFichier = "./Matrices/" + nomFichier + ".txt"; // Initialisation du chemin du fichier
 
